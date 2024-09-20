3990136d783867e6a469f32d4d0ef729e30f67a8
arleydemelquisedeclisbet <94724221+arleydemelquisedeclisbet@users.noreply.github.com>
2024-09-19 19:18:59 -0500

    Add adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,20 @@ import { defineConfig,envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
+
   env: {
     schema: {
       MOSTRAR_BOTON_COMPRAR: envField.boolean({ default: true, context: 'server', access: 'public' }),
       // ENDPOINT_SCORE_API: envField.string({context: 'client', access: 'public'}), // definir variable de entorno para obtener desde el cliente
       ENDPOINT_SCORE_API: envField.string({context: 'server', access: 'public'}),
     }
-  }
+  },
+
+  adapter: vercel()
 });
